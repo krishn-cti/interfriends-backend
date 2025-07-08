4dd76e5604e7353654165e89c03cd3e505cddf64
krish-cti <krishshukla.github@gmail.com>
2025-07-08 19:29:53 +0530

    changes in the stripe_helper

--- application/helpers/stripe_helper.php
+++ application/helpers/stripe_helper.php
@@ -1,31 +1,23 @@
 <?php
 if (!function_exists('AddStripe')) {
-
-	function AddStripe()
-	{
-		require_once(APPPATH . "libraries/stripe_lib/vendor/autoload.php");
-		\Stripe\Stripe::setApiKey($_ENV['STRIPE_SECRET_KEY_LIVE']); // Healthsteer Account Test Key
-
-	}
+    function AddStripe()
+    {
+        require_once(APPPATH . "libraries/stripe_lib/vendor/autoload.php");
+        \Stripe\Stripe::setApiKey($_ENV['STRIPE_SECRET_KEY_LIVE'] ?? '');
+    }
 }
 
 if (!function_exists('AddStripetest')) {
-
-	function AddStripetest()
-	{
-		require_once(APPPATH . "libraries/stripe_lib/vendor/autoload.php");
-		\Stripe\Stripe::setApiKey($_ENV['STRIPE_SECRET_KEY_TEST']); // Healthsteer Account Test Key
-
-
-
-
-	}
+    function AddStripetest()
+    {
+        require_once(APPPATH . "libraries/stripe_lib/vendor/autoload.php");
+        \Stripe\Stripe::setApiKey($_ENV['STRIPE_SECRET_KEY_TEST'] ?? '');
+    }
 }
+
 if (!function_exists('StripeKey')) {
-
-	function StripeKey()
-	{
-
-		return $_ENV['STRIPE_SECRET_KEY_TEST_1'];
-	}
+    function StripeKey()
+    {
+        return $_ENV['STRIPE_SECRET_KEY_TEST_1'] ?? '';
+    }
 }
